5728517516ecc710819b9c55fb0cb85033c85bd6
Aarya-Mangalore <apmangalore24@gmail.com>
2024-12-11 15:37:20 +0530

    do it!!!

--- src/routes/merchandise/+page.svelte
+++ src/routes/merchandise/+page.svelte
@@ -58,6 +58,15 @@
             staticImage="/images/merch/hoodie/front.jpg"
             galleryImages={hoodieImages}
             price="999"
+            link="https://forms.gle/JtsyFmPN1fCFpxRU9"
+        />
+
+        <ItemCard
+            Itemname="Combo (Hoodie+Tshirt)"
+            staticImage="/images/merch/combo.jpg"
+            galleryImages={Trend}
+            price="1300"
+            link="https://forms.gle/JtsyFmPN1fCFpxRU9"
         />
 
     </div>
