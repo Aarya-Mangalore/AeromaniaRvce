566e92172acdf01edf94cfd3c93098da3ed99cea
Aarya-Mangalore <apmangalore24@gmail.com>
2024-12-24 22:08:33 +0530

    results page added

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,1 +1,538 @@
-<!-- Keep it for future use??-->
+<script>
+    let events = [
+        {
+            id: "skyforge",
+            title: "Skyforge",
+            thumbnail: "/event/skyforge.jpg", // Replace with your image path
+            winners: {
+                team: "Team Lunar Codex",
+                members: [
+                    {
+                        name: "Syed Haseeb M",
+                        college: "Dayananda Sagar University",
+                        branch: "Aerospace Engineering",
+                    },
+                    {
+                        name: "Jaffer Sadiq",
+                        college: "Dayananda Sagar University",
+                        branch: "Aerospace Engineering",
+                    },
+                    {
+                        name: "Mohammed Safwan",
+                        college: "SVCE",
+                        branch: "Mechanical Engineering",
+                    },
+                    {
+                        name: "Mithun Sai K",
+                        college: "RV College of Engineering",
+                        branch: "Aerospace Engineering",
+                    },
+                ],
+            },
+            runnerUps: [
+                {
+                    team: "Team Stargazers",
+                    members: [
+                        {
+                            name: "Vishnu Charaan V",
+                            college: "RV College of Engineering",
+                            branch: "Electronics and Telecommunications Engineering",
+                        },
+                        {
+                            name: "Shyam Sreedaran",
+                            college: "RV College of Engineering",
+                            branch: "Electronics and Telecommunications Engineering",
+                        },
+                        {
+                            name: "Ravinuthala Sai",
+                            college: "RV College of Engineering",
+                            branch: "Electronics and Telecommunications Engineering",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: "aerocataclysm",
+            title: "Aerocataclysm",
+            thumbnail: "/event/aerocataclysm.jpg", // Replace with your image path
+            winners: {
+                team: "",
+                members: [
+                    {
+                        name: "Rohith D Raj",
+                        college: "RV College of Engineering",
+                        branch: "Electronics and Communication Engineering",
+                    },
+                    {
+                        name: "Vedith R",
+                        college: "RV College of Engineering",
+                        branch: "Aerospace Engineering",
+                    },
+                    {
+                        name: "P Vasudeva Acharya",
+                        college: "RV College of Engineering",
+                        branch: "Electronics and Communication Engineering",
+                    },
+                    {
+                        name: "Abhiram K",
+                        college: "RV College of Engineering",
+                        branch: "Information Science Engineering",
+                    },
+                ],
+            },
+            runnerUps: [
+                {
+                    team: "",
+                    members: [
+                        {
+                            name: "Udit H",
+                            college: "RV College of Engineering",
+                            branch: "Artificial Intelligence and Machine Learning",
+                        },
+                        {
+                            name: "Shreesha DR",
+                            college: "RV College of Engineering",
+                            branch: "Artificial Intelligence and Machine Learning",
+                        },
+                        {
+                            name: "Jerome Antony",
+                            college: "RV College of Engineering",
+                            branch: "Artificial Intelligence and Machine Learning",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: "design-dynamo",
+            title: "Design Dynamo",
+            thumbnail: "/event/designdynamo.jpg", // Replace with your image path
+            winners: {
+                team: "",
+                members: [
+                    {
+                        name: "Shamanth PV",
+                        college: "BMS College of Engineering",
+                        branch: "Aerospace Engineering",
+                    },
+                ],
+            },
+            runnerUps: [
+                {
+                    team: "",
+                    members: [
+                        {
+                            name: "Nikhil VP",
+                            college: "RV College of Engineering",
+                            branch: "Computer science and engineering (cybersecurity)",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: "aeroodessy",
+            title: "Aero odessy",
+            thumbnail: "/event/aeroodyssey.jpg", // Replace with your image path
+            winners: {
+                team: "D2",
+                members: [
+                    {
+                        name: "Abhinav M P",
+                        college: "RV College of Engineering",
+                        branch: "Electrical and Electronics Engineering",
+                    },
+                    {
+                        name: "Kishor R",
+                        college: "RV College of Engineering",
+                        branch: "Computer Science and Engineering (Data Science)",
+                    },
+                    {
+                        name: "Prajwal S M",
+                        college: "RV College of Engineering",
+                        branch: "Computer Science and Engineering",
+                    },
+                    {
+                        name: "Vaishnav",
+                        college: "RV College of Engineering",
+                        branch: "Electrical and Electronics Engineering",
+                    },
+                ],
+            },
+            runnerUps: [
+                {
+                    team: "B2",
+                    members: [
+                        {
+                            name: "Maanav",
+                            college: "RV College of Engineering",
+                            branch: "Industrial Engineering and Management",
+                        },
+                        {
+                            name: "Divyansh",
+                            college: "RV College of Engineering",
+                            branch: "Electronics and communications engineering",
+                        },
+                        {
+                            name: "Ainesh",
+                            college: "RV College of Engineering",
+                            branch: "Electronics and communications engineering",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: "aerogenesis",
+            title: "Aerogenesis",
+            thumbnail: "/event/aerogenisis.jpg", // Replace with your image path
+            winners: {
+                team: "Hack Mavericks",
+                members: [
+                    {
+                        name: "Hruthik N",
+                        college: "RV College of Engineering",
+                        branch: "Electronics and Communication Engineering",
+                    },
+                    {
+                        name: "Nandana K R",
+                        college: "BMS College of Engineering",
+                        branch: "Electronics and Communication Engineering",
+                    },
+                    {
+                        name: "Hrishikesh",
+                        college:
+                            "University of Visvesvaraya College of Engineering",
+                        branch: "Electronics and Communication Engineering",
+                    },
+                    {
+                        name: "Aditya",
+                        college: "BMS College of Engineering",
+                        branch: "Electronics and Communication Engineering",
+                    },
+                    {
+                        name: "Vishruth K S",
+                        college: "RV College of Engineering",
+                        branch: "Information Science and Engineering",
+                    },
+                ],
+            },
+            runnerUps: [
+                {
+                    team: "Raptor",
+                    members: [
+                        {
+                            name: "Shashank G",
+                            college: "RV College of Engineering",
+                            branch: "Mechanical Engineering",
+                        },
+                        {
+                            name: "Vinayak Verma",
+                            college: "RV College of Engineering",
+                            branch: "Mechanical Engineering",
+                        },
+                        {
+                            name: "Vignesh Chandramohan",
+                            college: "RV College of Engineering",
+                            branch: "Electronics and communications engineering",
+                        },
+                        {
+                            name: "Trisha M",
+                            college: "RNS Institute of Technology",
+                            branch: "Computer Science and Engineering",
+                        },
+                        {
+                            name: "Sai Siddarth B",
+                            college: "RNS Institute of Technology",
+                            branch: "Artificial Intelligence and Machine Learning",
+                        },
+                        {
+                            name: "Abhhay S Sharma",
+                            college:
+                                "National Institute of Technology, Karnataka",
+                            branch: "Mechanical engineering",
+                        },
+                    ],
+                },
+            ],
+        },
+    ];
+</script>
+
+<h1>Results</h1>
+
+<div class="results-container">
+    {#each events as event}
+        <div id={event.id} class="event-section">
+            <h1>{event.title}</h1>
+            <div class="results-content">
+                <img
+                    src={event.thumbnail}
+                    alt="{event.title} Thumbnail"
+                    class="event-thumbnail"
+                />
+
+                <div class="tables-container">
+                    <!-- Winners -->
+                    <div class="participants-table">
+                        <h1>Winner</h1>
+                        <div class="table-header">
+                            {event.winners.team || "Winner"}
+                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {#each event.winners.members as member}
+                                    <tr>
+                                        <td>{member.name}</td>
+                                        <td>{member.college}</td>
+                                        <td>{member.branch}</td>
+                                    </tr>
+                                {/each}
+                            </tbody>
+                        </table>
+                    </div>
+                    <br />
+                    <!-- Runner-Up Teams -->
+                    {#each event.runnerUps as runnerUp}
+                        <div class="runner-up-team">
+                            <h1>Runner-Up</h1>
+                            <div class="table-header">
+                                {runnerUp.team || "Runner-Up"}
+                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {#each runnerUp.members as member}
+                                        <tr>
+                                            <td>{member.name}</td>
+                                            <td>{member.college}</td>
+                                            <td>{member.branch}</td>
+                                        </tr>
+                                    {/each}
+                                </tbody>
+                            </table>
+                        </div>
+                    {/each}
+                </div>
+            </div>
+        </div>
+        <br />
+        <br />
+    {/each}
+</div>
+
+<style>
+    h1 {
+        font-size: 2rem;
+        text-align: center;
+        text-transform: uppercase;
+        margin-bottom: 20px;
+        color: #ffffff;
+    }
+
+    h1:hover {
+        color: #00ffff;
+        transform: scale(1.02);
+        text-decoration-skip-ink: auto;
+        transition: 0.3s;
+    }
+
+    .event-section h1 {
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.677);
+        border-top: 1px solid rgba(255, 255, 255, 0.677);
+    }
+
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        background-color: #000;
+        color: #0ff;
+        font-family: "CustomFont", sans-serif;
+    }
+
+    .results-container {
+        max-width: 80%;
+        margin: 0 auto;
+        padding: 2rem;
+    }
+
+    .event-section {
+        margin-bottom: 3rem;
+    }
+
+    .event-title {
+        text-align: center;
+        font-size: 2rem;
+        margin-bottom: 1rem;
+        font-family: "CustomFont", sans-serif;
+        text-transform: uppercase;
+    }
+
+    .event-title:hover {
+        color: #00ffff;
+        transform: scale(1.03);
+        transition: 0.3s;
+    }
+
+    .results-content {
+        display: flex;
+        gap: 2rem;
+        align-items: center;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    }
+
+    .event-thumbnail {
+        width: 100%;
+        max-width: 425px;
+        border-radius: 10px;
+        transition:
+            border 0.3s ease,
+            box-shadow 0.3s ease;
+    }
+
+    .event-thumbnail:hover {
+        border: 2px solid #0ff;
+        box-shadow: 0 0 15px #0ff;
+    }
+
+    .tables-container {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        flex: 1;
+        min-width: 300px;
+    }
+
+    .runner-up-team,
+    .participants-table {
+        background: rgba(0, 0, 0, 0);
+        padding: 1rem;
+        border-radius: 8px;
+        border: 2px solid transparent;
+        transition:
+            border 0.3s ease,
+            box-shadow 0.3s ease;
+    }
+
+    .runner-up-team:hover,
+    .participants-table:hover {
+        border: 2px solid #0ff;
+        box-shadow: 0 0 10px #0ff;
+    }
+
+    .table-header {
+        text-align: center;
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin: 0 auto;
+    }
+
+    th,
+    td {
+        padding: 0.8rem;
+        text-align: left;
+        width: 33%;
+    }
+
+    td {
+        color: #ffffff;
+        border: none;
+    }
+
+    tr:hover td {
+        transform: scale(1.02);
+        transition: 0.3s;
+        text-underline-offset: 5px;
+        border-bottom: 3px solid cyan;
+    }
+
+    @media (max-width: 768px) {
+        h1 {
+            font-size: 1.25rem;
+        }
+
+        .results-container {
+            padding: 0;
+            width: 100%;
+        }
+
+        .results-content {
+            flex-direction: column;
+            align-items: center;
+            gap: 20px;
+        }
+
+        .event-thumbnail {
+            margin-bottom: 2rem;
+            max-width: 250px;
+        }
+
+        .tables-container {
+            gap: 1rem;
+        }
+
+        .runner-up-team,
+        .participants-table {
+            padding: 0;
+        }
+
+        table {
+            width: 100%;
+        }
+
+        table,
+        th,
+        td {
+            text-align: center;
+        }
+
+        th,
+        td {
+            padding: 0.4rem;
+            text-align: center;
+            width: 25%;
+        }
+    }
+
+    @media (min-width: 1024px) and (max-width: 1440px) {
+        .results-content {
+            flex-direction: row;
+        }
+
+        table {
+            width: 95%;
+        }
+    }
+
+    @media (min-width: 1441px) {
+        .results-content {
+            flex-direction: row;
+        }
+
+        table {
+            width: 90%;
+        }
+    }
+</style>
